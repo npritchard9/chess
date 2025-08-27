98ff3184e147970d39e6452a2268787508ae4206
Noah Pritchard <noahpritchard9@gmail.com>
2025-08-27 01:17:26 -0400

    parse fen

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,7 @@
 open Core
 open Chess
 
-let () = printf "%Ld\n" @@ Moves.init_board ()
+let () =
+  let m = Fen.parse_fen "r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1" in
+  let board = Map.find_exn m '.' in
+  printf "%Ld\n" board
--- lib/fen.ml
+++ lib/fen.ml
@@ -4,5 +4,48 @@ open Core
 for each rank, squares begin from the first file and go to the 8th
 *)
 
-(* let parse fen = () *)
-let handle_char c = if Char.is_uppercase c then 1 else 0
+module CharMap = Map.Make (Char)
+
+let create_map () =
+  [
+    ('k', 0L);
+    ('q', 0L);
+    ('r', 0L);
+    ('b', 0L);
+    ('n', 0L);
+    ('p', 0L);
+    ('K', 0L);
+    ('Q', 0L);
+    ('R', 0L);
+    ('B', 0L);
+    ('N', 0L);
+    ('P', 0L);
+    ('.', 0L);
+  ]
+  |> Map.of_alist_exn (module Char)
+
+let parse_fen fen =
+  let m = create_map () in
+  let rec read s rank file m =
+    match s with
+    | [] -> m
+    | c :: rest -> (
+        match c with
+        | '/' -> read rest Int64.(rank - 1L) 1L m
+        | '8' -> read rest rank file m
+        | n when Char.is_digit n ->
+            read rest rank Int64.(file + (of_string @@ String.of_char c)) m
+        | p when Char.is_alpha p ->
+            let idx = Int64.((rank * 8L) - file) |> Int.of_int64_exn in
+            Map.update m p
+              ~f:
+                (Option.value_map ~default:0L ~f:(fun piece ->
+                     Int64.(piece lxor (1L lsl idx))))
+            |> Fn.flip Map.update '.'
+                 ~f:
+                   (Option.value_map ~default:0L ~f:(fun board ->
+                        Int64.(board lxor (1L lsl idx))))
+            |> read rest rank Int64.(file + 1L)
+        | _ -> failwith "unknown input")
+  in
+  read (String.to_list fen) 8L 1L m
